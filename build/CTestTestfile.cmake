e2b5468e548bd394a8d4316e9199d8f95104eaba
DavidAlvarez33 <davidalvarez33@hotmail.com>
2020-03-26 03:40:23 -0500

    Version 1

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/myrobot/Robotatto/src
+# Build directory: /home/myrobot/Robotatto/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("myrobot")
